b4a3caf94392cf843201bfb04623604e21720a32
Ojas-Poswal <ojasposwal@gmail.com>
2026-08-12 22:19:15 +0530

    feat: prevent duplicate patient registration

--- src/controllers/patient.controller.js
+++ src/controllers/patient.controller.js
@@ -3,6 +3,16 @@ import bcrypt from "bcrypt"
 
 const registerPatient = async (req,res)=>{
     const {fullName,email,phone,password} = req.body;
+
+    const existingPatient = await Patient.findOne({
+        $or : [{email},{phone}]
+    })
+
+    if(existingPatient){
+        return res.status(409).json({
+            message : "Email or phone already registered"
+        })
+    }
     
     const hashedPassword = await bcrypt.hash(password,10)
 
